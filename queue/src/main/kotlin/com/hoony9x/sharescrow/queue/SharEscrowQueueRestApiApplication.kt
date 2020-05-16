55e3e23b50499922c4b6156222ab3f3b1df4c5e8
Kihoon Han <me@hoony9x.com>
2020-05-16 17:32:26 +0900

    [Queue] Initial files generated by 'Spring Initializr'

---
+++ queue/src/main/kotlin/com/hoony9x/sharescrow/queue/SharEscrowQueueRestApiApplication.kt
@@ -0,0 +1,11 @@
+package com.hoony9x.sharescrow.queue
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SharEscrowQueueRestApiApplication
+
+fun main(args: Array<String>) {
+  runApplication<SharEscrowQueueRestApiApplication>(*args)
+}
