b6df70beb7c09bda67b891e8c5de7f98117e4aae
jaeyoon-lee <jaeyoon.leee@webtoonscorp.com>
2020-05-16 15:41:51 +0900

    batch 프로젝트 셋팅

--- sample/src/main/java/com/sharescrow/sample/sample/BatchConfig.java
+++ sample/src/main/java/com/sharescrow/sample/sample/BatchConfig.java
@@ -1,19 +1,31 @@
 package com.sharescrow.sample.sample;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 
 import javax.sql.DataSource;
 
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.batch.core.configuration.annotation.BatchConfigurer;
+import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.core.task.SyncTaskExecutor;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -24,11 +36,11 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Configuration
+@EnableBatchProcessing
 @EnableTransactionManagement
-@MapperScan(
-  basePackages = "com.sharescrow.sample.repository"
-)
-public class DataBaseConfig {
+@MapperScan(basePackages = "com.sharescrow.sample.repository")
+@ComponentScan(basePackages = {"com.sharescrow.sample"})
+public class BatchConfig {
   @Autowired
   private ApplicationContext applicationContext;
 
@@ -67,5 +79,34 @@ public class DataBaseConfig {
     sessionFactoryBean.setConfigLocation(applicationContext.getResource("classpath:/mybatis-config.xml"));
     sessionFactoryBean.setMapperLocations(applicationContext.getResources("classpath:/mapper/*Mapper.xml"));
     return sessionFactoryBean;
+  }
+
+  @Bean
+  public HttpMessageConverter<String> responseBodyConverter() {
+    return new StringHttpMessageConverter(Charset.forName("UTF-8"));
+  }
+
+  @Bean
+  BatchConfigurer configurer(@Qualifier("dataSource") DataSource dataSource) {
+    return new DefaultBatchConfigurer(dataSource);
+  }
+
+  @Bean
+  public StepBuilderFactory stepBuilderFactory(
+    JobRepository jobRepository,
+    @Qualifier("transactionManager") PlatformTransactionManager transactionManager
+  ) {
+    return new StepBuilderFactory(jobRepository, transactionManager);
+  }
+
+  @Bean(name = "syncJobLauncher")
+  public JobLauncher syncJobLauncher(JobRepository jobRepository) {
+    SimpleJobLauncher jobLauncher = new SimpleJobLauncher();
+    jobLauncher.setJobRepository(jobRepository);
+
+    SyncTaskExecutor taskExecutor = new SyncTaskExecutor();
+    jobLauncher.setTaskExecutor(taskExecutor);
+
+    return jobLauncher;
   }
 }
--- sample/src/main/java/com/sharescrow/sample/sample/SampleApplication.java
+++ sample/src/main/java/com/sharescrow/sample/sample/SampleApplication.java
@@ -2,22 +2,33 @@ package com.sharescrow.sample.sample;
 
 import java.nio.charset.Charset;
 
-import javax.servlet.Filter;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.core.task.SyncTaskExecutor;
+import org.springframework.transaction.PlatformTransactionManager;
 
-@Configuration
-@ComponentScan(basePackages = {"com.sharescrow.sample"})
+import javax.sql.DataSource;
+
+import org.springframework.batch.core.configuration.annotation.BatchConfigurer;
+import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+@EnableConfigurationProperties
+@Import({BatchConfig.class})
 @EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class})
 public class SampleApplication {
 
@@ -25,8 +36,4 @@ public class SampleApplication {
 		SpringApplication.run(SampleApplication.class, args);
 	}
 
-	@Bean
-	public HttpMessageConverter<String> responseBodyConverter() {
-		return new StringHttpMessageConverter(Charset.forName("UTF-8"));
-	}
 }
