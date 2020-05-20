9ec6ca09a48197205f03613fc0edb4e433fdd189
JoHoYoung <whghdud17@naver.com>
2020-05-21 03:59:08 +0900

    rm useless import

--- payment/src/main/java/com/sharescrow/payment/service/auth/AuthService.java
+++ payment/src/main/java/com/sharescrow/payment/service/auth/AuthService.java
@@ -1,11 +1,5 @@
 package com.sharescrow.payment.service.auth;
 
-import com.sharescrow.payment.ErrorCode;
-import com.sharescrow.payment.exception.InvalidTokenException;
-import com.sharescrow.payment.exception.TokenExpiredException;
-import io.jsonwebtoken.*;
-
-import java.util.Date;
 
 public interface AuthService {
 	String accessToken(String subject);
--- payment/src/main/java/com/sharescrow/payment/service/auth/HandlerInterceptorAdapterImpl.java
+++ payment/src/main/java/com/sharescrow/payment/service/auth/HandlerInterceptorAdapterImpl.java
@@ -7,8 +7,6 @@ import com.sharescrow.payment.exception.InvalidTokenException;
 import com.sharescrow.payment.exception.UnauthorizedAccessException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
