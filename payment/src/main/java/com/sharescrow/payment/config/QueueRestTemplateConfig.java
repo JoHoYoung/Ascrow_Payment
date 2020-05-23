6e90cdc5f155e289699e25c99817254e37edcdb1
JoHoYoung <whghdud17@naver.com>
2020-05-23 14:32:49 +0900

    rm bufferingfactory

--- payment/src/main/java/com/sharescrow/payment/config/QueueRestTemplateConfig.java
+++ payment/src/main/java/com/sharescrow/payment/config/QueueRestTemplateConfig.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.BufferingClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
