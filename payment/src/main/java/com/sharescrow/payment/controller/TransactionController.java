8ed67dabad03e69bd2495295ee2a6561c6a606c9
JoHoYoung <whghdud17@naver.com>
2020-05-23 14:34:20 +0900

    add DataState, and edit setter

--- payment/src/main/java/com/sharescrow/payment/controller/TransactionController.java
+++ payment/src/main/java/com/sharescrow/payment/controller/TransactionController.java
@@ -1,8 +1,11 @@
 package com.sharescrow.payment.controller;
 
+import javax.xml.crypto.Data;
+
 import com.sharescrow.payment.context.HistoryStage;
 import com.sharescrow.payment.context.pay.request.kakao.KakaoPayApiCancelRequest;
 import com.sharescrow.payment.context.pay.response.kakao.KakaoPayApiCancelResponse;
+import com.sharescrow.payment.model.DataState;
 import com.sharescrow.payment.model.Order;
 import com.sharescrow.payment.model.Transaction;
 import com.sharescrow.payment.response.BaseResponse;
@@ -15,6 +18,7 @@ import com.sharescrow.payment.service.apiService.ProductApiService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,6 +40,7 @@ public class TransactionController {
 	ProductApiService productApiService;
 
 	// in kakaopay, when user cancel call this
+	@Transactional
 	@PostMapping("/kakaoPay/cancel/{transactionId}")
 	public ResponseEntity<BaseResponse> kakaoPayCancel(@PathVariable("transactionId")String transactionId) {
 		Order order = orderService.getOrderByTransactionId(transactionId);
@@ -43,9 +48,8 @@ public class TransactionController {
 
 		historyService.saveHistory(order, HistoryStage.TRANSACTION_CANCEL_DONE);
 
-		transaction.setDeleted();
-		order.setDeleted();
-
+		transaction.setState(DataState.DELETED);
+		transaction.setState(DataState.DELETED);
 		transactionService.update(transaction.getId(), transaction);
 		orderService.updateOrder(order);
 
@@ -54,6 +58,8 @@ public class TransactionController {
 	}
 
 	// in kakaopay, when failure call this
+
+	@Transactional
 	@PostMapping("/kakaoPay/fail/{transactionId}")
 	public ResponseEntity<BaseResponse> kakaoPayFailure(@PathVariable("transactionId")String transactionId) {
 		Order order = orderService.getOrderByTransactionId(transactionId);
@@ -61,11 +67,11 @@ public class TransactionController {
 
 		historyService.saveHistory(order, HistoryStage.TRANSACTION_CANCEL_DONE);
 
-		transaction.setDeleted();
-		order.setDeleted();
+		transaction.setState(DataState.DELETED);
+		order.setState(DataState.DELETED);
 
-		transactionService.update(transaction.getId(), transaction);
 		orderService.updateOrder(order);
+		transactionService.update(transaction.getId(), transaction);
 
 		productApiService.cancelOrder(order);
 		return new ResponseEntity<>(new BaseResponse(), HttpStatus.OK);
